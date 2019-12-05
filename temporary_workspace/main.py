8fe65afc966ac4de166ce209759e3679d937c184
Rituraj Ingale <ingalerd@gmail.com>
2019-12-05 13:10:24 +0530

    Start
    TO DO Steps
    Planing

---
+++ temporary_workspace/main.py
@@ -0,0 +1,12 @@
+"""
+
+===> TO DO
+
+1 -> Read the Audio File
+2 -> Run it on speech to text API
+3 -> Save Transcript as a JSON text file
+4 -> Edit the Audio File
+5 -> Save the File
+6 -> Send the Path to the Web App
+
+"""
