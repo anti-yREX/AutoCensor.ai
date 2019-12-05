7b9ecd250aa64709cfd0523a835b94284af6477c
Rituraj Ingale <ingalerd@gmail.com>
2019-12-05 14:26:58 +0530

    convertion of mp3  to wav - Done
    The Audio is converted  from mp3 to wav.
    
    Next Tasks are as Follows:
    1. Read the Audio File
    4. Edit the Signals of the File
    5. Save the File

--- temporary_workspace/main.py
+++ temporary_workspace/main.py
@@ -3,6 +3,7 @@
 ===> TO DO
 
 1 -> Read the Audio File
+    1.1-> If audio in .mp3 convert to .wav  (Done)
 2 -> Run it on speech to text API
 3 -> Save Transcript as a JSON text file
 4 -> Edit the Audio File
@@ -10,3 +11,36 @@
 6 -> Send the Path to the Web App
 
 """
+def convert_mp3_to_wav(src):
+    from os import path
+    from pydub import AudioSegment
+
+    # files                                                                         
+    dst = make_dst(src)
+    print(src + "\n" + dst)
+    # convert wav to mp3                                                            
+    sound = AudioSegment.from_mp3(src)
+    sound.export(dst, format="wav")
+    return dst
+
+def make_dst(src):
+    i = len(src) - 1
+    while(i >= 0):
+        if(src[i] == '.'):
+            dot = i
+        i -= 1
+    
+    dst = ""
+    
+    for i in  range(0,dot):
+        dst += src[i]
+    dst  = dst + "_wav.wav"
+    return dst
+
+
+
+#Next Tasks
+#1 Read the Audio File
+#4 Edit the Audio File
+#5 Save the Audio File
+
