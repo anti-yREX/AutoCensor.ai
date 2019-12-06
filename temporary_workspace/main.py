8f929e84b6995cb77586fe21e139d72267f9fe27
Rituraj Ingale <ingalerd@gmail.com>
2019-12-06 21:04:49 +0530

    Editing Module Done
    beep() is ready to use.
    Only available for single timeslot.
    
    Would make it available for Multiple Slots Later
    
    Next Steps:
    #1.1 Convert WAV to RAW
    #2 Run Audio on API
    #3 Check the Transcript
    #6 Save in JSON

--- temporary_workspace/main.py
+++ temporary_workspace/main.py
@@ -2,27 +2,17 @@
 
 ===> TO DO
 
-1 -> Read the Audio File
-    1.1-> If audio in .mp3 convert to .wav  (Done)
+1 -> Read the Audio File (Done)
+    1.1-> If audio in .mp3 convert to .wav  (Done) (Saved in temp.py)
 2 -> Run it on speech to text API
-3 -> Save Transcript as a JSON text file
-4 -> Edit the Audio File
-5 -> Save the File
-6 -> Send the Path to the Web App
+3 -> Check the Transcript for Wrong Words
+4 -> Edit the Audio File (Done)
+5 -> Save the File (Done)
+6 -> Save the marked word into a JSON text file
 
 """
-from os import path
-from pydub import AudioSegment
 
-def convert_mp3_to_wav(src):
-    # files                                                                         
-    dst = make_dst(src)
-    print(src + "\n" + dst)
-    # convert wav to mp3                                                            
-    sound = AudioSegment.from_mp3(src)
-    sound.export(dst, format="wav")
-    return dst
-
+import wave
 def make_dst(src):
     i = len(src) - 1
     while(i >= 0):
@@ -34,21 +24,45 @@ def make_dst(src):
     
     for i in  range(0,dot):
         dst += src[i]
-    dst  = dst + "_wav.wav"
+    dst  = dst + "_edited.wav"
     return dst
 
-src = "audios/audio1.mp3"
-src = convert_mp3_to_wav(src)
 
 
+def beep(src, strt, end):
+    dst = make_dst(src)
+    
+    beep = "audios/unit_beep.wav"
+    b = wave.open(beep, 'r')
 
-"""
-Audio 1 - 700  : 2000
-Audio 2 - 3000 : end
-"""
+    #1 Read the Audio File
+    a = wave.open(src, 'r')
+    t = wave.open(dst, 'w')
+    t.setparams(a.getparams())
+
+    #4 Edit the Audio File
+    st = int(strt * 44100)
+    ed = int(end * 44100)
+
+    t.writeframes(a.readframes(st))
+
+    while(t.tell() <= ed):
+        b.setpos(0)
+        t.writeframes(b.readframes(50))
+
+    a.setpos(ed)
+    t.writeframes(a.readframes(a.getnframes()-ed))
+
+    #5 Save the Audio File
+    b.close()
+    t.close()
+    a.close()
+    return dst
+
+src = "audios/audio1_wav.wav"
+src = beep(src, 0.983, 2.1)
+print(src)
+
+
 
 #Next Tasks
-#1 Read the Audio File
-#4 Edit the Audio File
-#5 Save the Audio File
-
--- temporary_workspace/temp.py
+++ temporary_workspace/temp.py
@@ -1,9 +1,10 @@
-import wave
+"""
+#Next Tasks
+#1 Read the Audio File
+#4 Edit the Audio File
+#5 Save the Audio File
 
-"""
-Audio 1 - 1000ms - 2100ms
-"""
-    
+import wave
 beep = "audios/unit_beep.wav"
 b = wave.open(beep, 'r')
 #Frame a Milisecond are different .. Calculate in Frames  1000ms = 44100 frames 
@@ -19,6 +20,7 @@ t.setparams(a.getparams())
 st = int(0.938 * 44100)
 ed = int(2.1 * 44100)
 t.writeframes(a.readframes(st))
+
 while(t.tell() <= ed):
     b.setpos(0)
     t.writeframes(b.readframes(50))
@@ -27,3 +29,22 @@ t.writeframes(a.readframes(a.getnframes()-ed))
 b.close()
 t.close()
 a.close()
+"""
+"""
+#  Convert MP3 to WAV
+
+from os import path
+from pydub import AudioSegment
+
+def convert_mp3_to_wav(src):
+    # files                                                                         
+    dst = make_dst(src)
+    print(src + "\n" + dst)
+    # convert wav to mp3                                                            
+    sound = AudioSegment.from_mp3(src)
+    sound.export(dst, format="wav")
+    return dst
+src = "audios/audio1.mp3"
+src = convert_mp3_to_wav(src)
+
+"""
