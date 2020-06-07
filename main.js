c01b796090bb34e2bf332ba12c8f22fe8b48d3df
sairaj mote <sairajmote3@gmail.com>
2020-06-07 12:41:07 +0530

    new UI

--- main.js
+++ main.js
@@ -1,59 +1,294 @@
 //jshint esversion: 6
 
-let original_audio;
-function dropHandler(ev) {
-    console.log('File(s) dropped');
-  
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-  
-    if (ev.dataTransfer.items) {
-      // Use DataTransferItemList interface to access the file(s)
-      for (let i = 0; i < ev.dataTransfer.items.length; i++) {
-        // If dropped items aren't files, reject them
-        if (ev.dataTransfer.items[i].kind === 'file') {
-          var file = ev.dataTransfer.items[i].getAsFile();
-            console.log('... file[' + i + '].name = ' + file.name);
-          document.getElementById('audio_name').textContent = file.name;
-          original_audio = file.name;
-          call(file.name);
+document.getElementById('drop_zone').addEventListener('drop', dropHandler)
+document.getElementById('drop_zone').addEventListener('dragover', dragOverHandler)
+document.getElementById('drop_zone').addEventListener('dragleave', dragLeaveHandler)
 
-          let hiddenElemennts = document.querySelectorAll('.hide-on-drop');
-          hiddenElemennts.forEach((element) => {
-            element.classList.add('hide');
-          });
-          document.getElementById('loader').classList.add('rotate');
-        }
-      }
-    } else {
-      // Use DataTransfer interface to access the file(s)
-      for (let i = 0; i < ev.dataTransfer.files.length; i++) {
-        console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-      }
-  }
+let fileInputBtn = document.getElementById('select_file_btn'),
+	startCensorBtn = document.getElementById('start_censor_btn'),
+	getCensorWordContainer = document.getElementById('get_censor_word_container'),
+	originalFileName = '';
+fileInputBtn.addEventListener('change', function() {
+	originalFileName = this.files[0].name;
+	showCensorWordInput()
+})
+
+function dropHandler(e) {
+	e.preventDefault();
+	originalFileName = e.dataTransfer.files[0].name;
+	showCensorWordInput()
 }
-  
+
+startCensorBtn.addEventListener('click', e => {
+	proccessRequest()
+})
+document.getElementById('get_censor_word').addEventListener('keyup', e => {
+	if(e.key === 'Enter')
+		proccessRequest()
+})
+
+function proccessRequest() {
+	let censoredWord = document.getElementById('get_censor_word').value;
+	if (censoredWord.trim() === '')
+		alert('Please enter a word to censor')
+	else {
+		document.getElementById('main_page').classList.add('hide')
+		document.getElementById('result_page').classList.remove('hide')
+		//	sendFile(originalFileName, censoredWord)
+	}
+}
+
+let words = [
+	{
+		word: 'some',
+		censored: false
+	},
+	{
+		word: 'something',
+		censored: false
+	},
+	{
+		word: 'someth',
+		censored: true
+	},
+	{
+		word: 'something',
+		censored: false
+	},
+	{
+		word: 'something',
+		censored: false
+	},
+	{
+		word: 'something',
+		censored: false
+	},
+]
+
+let frag = document.createDocumentFragment();
+function createWord(word, status) {
+	let card = document.createElement('div')
+	card.classList.add('word-card')
+	card.textContent = word;
+	if (status)
+		card.classList.add('censored')
+	return card;
+}
+
+function renderWords(words) {
+	document.getElementById('word_container').innerHTML = ''
+	words.forEach(word => {
+		frag.append(createWord(word.word, word.censored))
+	})
+	document.getElementById('word_container').append(frag)
+}
+renderWords(words)
+
 function dragOverHandler(ev) {
-    console.log('File(s) in drop zone'); 
-    ev.preventDefault();
+	this.classList.add('highlight')
+	ev.preventDefault();
+}
+function dragLeaveHandler(ev) {
+	this.classList.remove('highlight')
+	ev.preventDefault();
+}
+function showCensorWordInput() {
+	document.querySelectorAll('.hide-on-file-selected').forEach(el => {
+		el.classList.add('hide')
+	})
+	getCensorWordContainer.classList.remove('hide')
+	document.getElementById('selected_file_name').textContent = originalFileName
+}
+async function sendFile(fileName, word) {
+	const response = await fetch('http://localhost:5000/api/', {
+		method: 'post',
+		body: JSON.stringify({ "message": { fileName: fileName, word: word } })
+	}),
+		data = await response.json();
+	document.getElementById("originalFileName").setAttribute('src', `audios/${originalFileName}`);
+	document.getElementById('loader').classList.remove('rotate');
+	document.querySelector('.show-on-drop').classList.remove('hide');
+	if (data.message != "No_Censor") {
+		document.getElementById("censored_audio").setAttribute('src', data.message);
+	} else {
+		document.getElementById("no").textContent = "No Censor Done on Audio"
+		document.getElementById("censored_audio").setAttribute('src', `audios/${originalFileName}`);
+	}
 }
 
-function call(message){
-  $.ajax({
-    url: "http://localhost:5000/api/",
-    type: "POST",
-    contentType: "application/json",
-    data: JSON.stringify({ "message": message })
-  }).done(function (data) {
-    document.getElementById("original_audio").setAttribute('src', `audios/${original_audio}`);
-    document.getElementById('loader').classList.remove('rotate');
-    document.querySelector('.show-on-drop').classList.remove('hide');
-    // console.log(original_audio)
-    if(data.message != "No_Censor"){
-      document.getElementById("censored_audio").setAttribute('src', data.message);
-    } else {
-      document.getElementById("no").innerHTML = "No Censor Done on Audio"
-      document.getElementById("censored_audio").setAttribute('src', `audios/${original_audio}`);
-    }
-  });
+/*function call(message) {
+	$.ajax({
+		url: "http://localhost:5000/api/",
+		type: "POST",
+		contentType: "application/json",
+		data: JSON.stringify({ "message": message })
+	}).done(function (data) {
+		document.getElementById("originalFileName").setAttribute('src', `audios/${originalFileName}`);
+		document.getElementById('loader').classList.remove('rotate');
+		document.querySelector('.show-on-drop').classList.remove('hide');
+		// console.log(originalFileName)
+		if (data.message != "No_Censor") {
+			document.getElementById("censored_audio").setAttribute('src', data.message);
+		} else {
+			document.getElementById("no").textContent = "No Censor Done on Audio"
+			document.getElementById("censored_audio").setAttribute('src', `audios/${originalFileName}`);
+		}
+	});
+}*/
+
+
+// web components
+
+const smAudio = document.createElement('template')
+smAudio.innerHTML = `
+<style>
+.audio{
+  position: relative;
+  display: -webkit-box;
+  display: flex;
+  -webkit-box-align: center;
+          align-items: center;
+  -webkit-user-select: none;
+          user-select: none;
+  padding: 0.6rem;
+  border-radius: 0.2rem;
+  background: rgba(var(--text), 0.08);
+  overflow: hidden;
 }
+
+.hide {
+  display: none;
+}
+
+.icon {
+  stroke: rgba(var(--text), 1);
+  stroke-width: 6;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  overflow: visible;
+  height: 2.1rem;
+  width: 2.1rem;
+  padding: 0.6rem;
+  cursor: pointer;
+  border-radius: 2rem;
+  margin-right: 0.5rem;
+}
+
+.icon:hover {
+  background: rgba(var(--text), 0.1);
+}
+
+audio {
+  display: none;
+}
+
+.track {
+  position: absolute;
+  height: 0.2rem;
+  bottom: 0;
+  background: var(--primary-color);
+  left: 0;
+  -webkit-transition: width 0.2s;
+  transition: width 0.2s;
+  pointer-events: none;
+  z-index: 0;
+  border-radius: 0.2rem;
+}
+
+.disabled {
+  opacity: 0.6;
+  pointer-events: none;
+}
+</style>
+	<div class="audio">
+		<svg class="icon play" viewBox="-6 0 64 64">
+			<title>play</title>
+			<path d="M57.12,26.79,12.88,1.31a6,6,0,0,0-9,5.21v51a6,6,0,0,0,9,5.21L57.12,37.21A6,6,0,0,0,57.12,26.79Z"/>
+		</svg>
+		<svg class="icon pause hide" viewBox="0 0 64 64">
+			<title>pause</title>
+			<line x1="17.5" x2="17.5" y2="64"/>
+			<line x1="46.5" x2="46.5" y2="64"/>
+		</svg>
+		<h5 class="current-time"></h5> / <h5 class="duration"></h5>
+		<audio src=""></audio>
+		<div class="track"></div>
+	</audio>
+`;
+
+customElements.define('sm-audio', class extends HTMLElement{
+	constructor() {
+		super();
+		this.append(smAudio.content.cloneNode(true))
+
+		this.playing = false;
+	}
+	static get observedAttributes() {
+		return ['src']
+	}
+	play() {
+		this.audio.play()
+		this.playing = false;
+		this.pauseBtn.classList.remove('hide')
+		this.playBtn.classList.add('hide')
+	}
+	pause() {
+		this.audio.pause()
+		this.playing = true;
+		this.pauseBtn.classList.add('hide')
+		this.playBtn.classList.remove('hide')
+	}
+
+	get isPlaying() {
+		return this.playing;
+	}
+	connectedCallback() {
+		this.playBtn = this.querySelector('.play');
+		this.pauseBtn = this.querySelector('.pause');
+		this.audio = this.querySelector('audio')
+		this.playBtn.addEventListener('click', e => {
+			this.play()
+		})
+		this.pauseBtn.addEventListener('click', e => {
+			this.pause()
+		})
+		this.audio.addEventListener('ended', e => {
+			this.pause()
+		})
+		let width;
+		let observer = new IntersectionObserver((entries, observer) => {
+				if (entries[0].isIntersecting)
+					width = this.querySelector('.audio').offsetWidth;
+		}, {
+			threshold: 1
+		})
+		observer.observe(this)
+		this.audio.addEventListener('timeupdate', e => {
+			let minutes = Math.floor(this.audio.currentTime / 60),
+				seconds = Math.floor(this.audio.currentTime),
+				y = seconds < 10 ? "0" + seconds : seconds;
+			this.querySelector('.current-time').textContent = `${minutes}:${y}`
+			this.querySelector('.track').style.width = (width / this.audio.duration) * this.audio.currentTime + 'px'
+		})
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (oldValue !== newValue) {
+			if (name === 'src') {
+				if (this.hasAttribute('src') && newValue.trim() !== '') {
+					this.querySelector('audio').src = newValue;
+					this.querySelector('audio').onloadedmetadata = () => {
+						let minutes = Math.floor(this.audio.duration / 60),
+							seconds = Math.floor(this.audio.duration),
+							y = seconds < 10 ? "0" + seconds : seconds;
+						this.querySelector('.duration').textContent = `${minutes}:${y}`;
+					}
+				}
+				else
+					this.classList.add('disabled')
+			}
+		}
+	}
+})
+
